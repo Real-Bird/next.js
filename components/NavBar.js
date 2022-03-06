ce3fbbecf355cde0ef17ea9839d476e8baf9be13
RealBird <marbil815@gmail.com>
2022-03-06 21:06:40 +0900

    22-03-06 ssr, router

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,23 +1,28 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 // import styles from "./NavBar.module.css";
-
+import film from "../public/film.png";
 export default function NvaBar() {
   const router = useRouter();
   return (
     <nav>
-      <svg scr="/vercel.svg" />
       <div>
-        <Link href="/">
-          {/* <a className={styles.nav}>Home</a> */}
-          {/* <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</a> */}
-          <a className={router.pathname === "/" ? "active" : ""}>Home</a>
-        </Link>
+        <Image className="img" src={film} width={400} height={200} alt="film" />
+        <div>
+          <Link href="/">
+            {/* <a className={styles.nav}>Home</a> */}
+            {/* <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</a> */}
+            <a className={router.pathname === "/" ? "active" : ""}>Home</a>
+          </Link>
 
-        <Link href="/about">
-          {/* <a className={[styles.link, router.pathname === "/about" ? styles.active : ""].join(" ")}>About</a> */}
-          <a className={router.pathname === "/about" ? "active" : ""}>About</a>
-        </Link>
+          <Link href="/about">
+            {/* <a className={[styles.link, router.pathname === "/about" ? styles.active : ""].join(" ")}>About</a> */}
+            <a className={router.pathname === "/about" ? "active" : ""}>
+              About
+            </a>
+          </Link>
+        </div>
       </div>
       <style jsx>{`
         nav {
@@ -30,9 +35,11 @@ export default function NvaBar() {
           box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
             rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
         }
-        img {
-          max-width: 100px;
-          margin-bottom: 5px;
+        .img {
+          position: absolute;
+          z-index: -1;
+          max-width: 400px;
+          margin-bottom: 10px;
         }
         nav a {
           font-weight: 600;
@@ -42,8 +49,19 @@ export default function NvaBar() {
           color: tomato;
         }
         nav div {
+          position: relative;
           display: flex;
           gap: 10px;
+          flex-direction: column;
+          flex-wrap: wrap;
+        }
+
+        nav div div {
+          display: flex;
+          gap: 10px;
+          flex-direction: row;
+          justify-content: center;
+          transform: translateY(-125px);
         }
       `}</style>
     </nav>
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,47 @@
 // import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import HeadTitle from "../components/HeadTitle";
 // import NvaBar from "../components/NavBar";
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch("/api/movies")).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        // `/movies/${id}`
+        pathname: `/movies/${id}`,
+        query: {
+          title: title,
+        },
+      },
+      `/movies/${id}`
+    );
+  };
+  // const [movies, setMovies] = useState();
+  // useEffect(() => {
+  //   (async () => {
+  //     const { results } = await (await fetch("/api/movies")).json();
+  //     setMovies(results);
+  //   })();
+  // }, []);
   return (
     <div className="container">
       <HeadTitle title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
-        <div className="movie" key={movie.id}>
+      {/* {!movies && <h4>Loading...</h4>} */}
+      {results?.map((movie) => (
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <Link href={`/movie/${movie.id}`}>
+            <a>
+              <h4>{movie.original_title}</h4>
+            </a>
+          </Link>
         </div>
       ))}
       <style jsx>{`
@@ -33,6 +56,19 @@ export default function Home() {
           border-radius: 12px;
           transition: transform 0.2s ease-in-out;
           box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+          animation: fadeIn 2s 1;
+        }
+        @keyframes fadeIn {
+          0% {
+            opacity: 0;
+          }
+
+          100% {
+            opacity: 1;
+          }
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie:hover img {
           transform: scale(1.05) translateY(-10px);
@@ -44,4 +80,15 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch("http://localhost:3000/api/movies")
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
